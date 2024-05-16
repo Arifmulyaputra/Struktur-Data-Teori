5452fa4dfc9803645c558f1c281c1346ef2bdd9d
Arif Mulya Putra <arif.mly15ptr@gmail.com>
2024-05-16 23:12:48 +0700

    first

---
+++ CPMK 2 SubCPMK2/Soal 4.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void mergeAndSort(int A[], int sizeA, int B[], int sizeB) {
+    int mergedSize = sizeA + sizeB;
+    int merged[mergedSize];
+
+    for (int i = 0; i < sizeA; ++i) {
+        merged[i] = A[i];
+    }
+    for (int i = 0; i < sizeB; ++i) {
+        merged[sizeA + i] = B[i];
+    }
+
+
+    sort(merged, merged + mergedSize);
+
+
+    for (int i = 0; i < mergedSize; ++i) {
+        A[i] = merged[i];
+    }
+}
+
+int main() {
+    int A[] = {1, 3, 5, 7, 9};
+    int sizeA = sizeof(A) / sizeof(A[0]);
+
+    int B[] = {2, 4, 6, 8, 10};
+    int sizeB = sizeof(B) / sizeof(B[0]);
+
+    mergeAndSort(A, sizeA, B, sizeB);
+
+    cout << "Array A setelah digabungkan dan diurutkan: ";
+    for (int i = 0; i < sizeA + sizeB; ++i) {
+        cout << A[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
